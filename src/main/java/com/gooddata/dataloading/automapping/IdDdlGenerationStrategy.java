2b34d7e36aa85cb655f34d64522e2453a98b88d8
Juraj Martinka <juraj.martinka@gooddata.com>
2014-11-19 10:14:13 +0100

    DL automapping strategies - test tunning

--- src/main/java/com/gooddata/dataloading/automapping/IdDdlGenerationStrategy.java
+++ src/main/java/com/gooddata/dataloading/automapping/IdDdlGenerationStrategy.java
@@ -3,8 +3,10 @@ package com.gooddata.dataloading.automapping;
 import com.gooddata.service.model.project.model.ProjectModel;
 
 public class IdDdlGenerationStrategy implements DdlGenerationStrategy {
+
     @Override
     public String generateDdl(ProjectModel projectModel) {
-        return null;
+        return "CREATE TABLE";
     }
+
 }
--- src/test/java/com/gooddata/dataloading/automapping/IdDdlGenerationStrategyTest.java
+++ src/test/java/com/gooddata/dataloading/automapping/IdDdlGenerationStrategyTest.java
@@ -1,19 +1,19 @@
 package com.gooddata.dataloading.automapping;
 
-import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.isEmptyString;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import com.gooddata.json.abstractobject.JsonObjectMapper;
 import com.gooddata.service.model.project.ProjectModelView;
-import com.gooddata.service.model.project.model.PmDataset;
-import com.gooddata.service.model.project.model.PmDateDimension;
 import com.gooddata.service.model.project.model.ProjectModel;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
 
 public class IdDdlGenerationStrategyTest {
 
@@ -22,26 +22,32 @@ public class IdDdlGenerationStrategyTest {
     private DdlGenerationStrategy idStrategy = new IdDdlGenerationStrategy();
 
     @Test
-    public void dummyProjectModel() throws IOException {
+    public void generateDdlForSimpleDatasetWithOneFactAndAttribute() throws IOException {
 
-        final ProjectModel dummyModel = new ProjectModel(
-                asList(
-                        new PmDataset("person", "Person", null, null, null, null, null)),
-                Collections.<PmDateDimension>emptyList());
-        final String dummyDdl = idStrategy.generateDdl(dummyModel);
-        assertNotNull("dummyDdl should not be null!", dummyDdl);
+        final String personDdl = idStrategy.generateDdl(readModel("person_model.json"));
+
+        assertThat(personDdl, not(isEmptyString()));
+        assertThat(personDdl, is("CREATE TABLE person"));
     }
+
+
 
     @Test
     public void goodSalesProjectModel() throws IOException {
-        final ProjectModelView goodSalesModel = objectMapper.readValue(readResource("goodsales_model.json"), ProjectModelView.class);
-        assertNotNull("goodSalesModel should not be null!", goodSalesModel);
-
-        idStrategy.generateDdl(goodSalesModel.getModel());
+        idStrategy.generateDdl(readModel("goodsales_model.json"));
 
     }
 
-    private InputStream readResource(String resource) {
-        return getClass().getClassLoader().getResourceAsStream(resource);
+
+    //--------------------------------------------------- HELPER METHODS -----------------------------------------------
+
+    private ProjectModel readModel(String modelJsonPath) {
+        final InputStream jsonStream = getClass().getClassLoader().getResourceAsStream(modelJsonPath);
+        assertNotNull("resource should not be null!", jsonStream);
+        try {
+            return objectMapper.readValue(jsonStream, ProjectModelView.class).getModel();
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read model json '" + modelJsonPath + "'", e);
+        }
     }
 }
